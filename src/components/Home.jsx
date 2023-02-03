61349b42785687244ed47560563ab2f70118a911
rohityellapu <rohithyellapu24@gmail.com>
2023-02-03 12:00:51 +0530

    made some css in input and th

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,19 +42,19 @@ function Home() {
                 <Nav user={ user } />
                 <SideBar todos={ todos } />
                 <main className='lg:ml-56 lg:pl-28 mt-48'>
-                    { ongoingErr && <p className='text-center text-3xl font-semibold text-red-600'>Finish or Pause the ongoing task first.</p> }
+                    { ongoingErr && <p className='text-center text-xl lg:text-3xl font-semibold text-red-600'>Finish or Pause the ongoing task first.</p> }
                     <div className="addtask flex gap-4 justify-center m-4">
-                        <input className='p-2 rounded-md focus:outline-green-500' type="text" value={ addActivity } onChange={ (e) => setActivity(e.target.value) } name="activity" id="" placeholder='Task Name' />
+                        <input className='p-2 rounded-md bg-green-100 focus:outline-green-500' type="text" value={ addActivity } onChange={ (e) => setActivity(e.target.value) } name="activity" id="" placeholder='Task Name' />
                         <button disabled={ addActivity.length == 0 } className='p-2 bg-violet-500 hover:bg-violet-800 rounded-md disabled:bg-violet-500 disabled:cursor-not-allowed' onClick={ handleAdd }>Add activity</button>
                     </div>
                     <div className="tasks flex justify-center">
                         <table className='table-auto'>
                             <thead>
                                 <tr className='m-2 p-2 bg-gray-600'>
-                                    <th className='w-24 lg:w-56 text-2xl p-2 m-2 border-2'>Activity</th>
-                                    <th className='w-24 lg:w-56 text-2xl p-2 m-2 border-2'>Status</th>
-                                    <th className='w-24 lg:w-56 text-2xl p-2 m-2 border-2'>Time Taken</th>
-                                    <th className='w-24 lg:w-56 text-2xl p-2 m-2 border-2'>Action</th>
+                                    <th className='w-20 lg:w-56 text-lg lg:text-2xl p-2 m-2 border-2'>Activity</th>
+                                    <th className='w-20 lg:w-56 text-lg lg:text-2xl p-2 m-2 border-2'>Status</th>
+                                    <th className='w-20 lg:w-56 text-lg lg:text-2xl p-2 m-2 border-2'>Time Taken (HH:MM:SS) </th>
+                                    <th className='w-20 lg:w-56 text-lg lg:text-2xl p-2 m-2 border-2'>Action</th>
                                 </tr>
                             </thead>
                             <tbody className=''>
