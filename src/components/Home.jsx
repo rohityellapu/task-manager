af664ab9b0025a81dea0b9e1d282e27494ba9a2f
rohityellapu <rohithyellapu24@gmail.com>
2023-02-03 11:54:23 +0530

    made responsive for mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,49 +2,47 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, Navigate } from 'react-router-dom';
 import Nav from './Nav';
 import SideBar from './SideBar';
-const apiUrl = 'https://todo-api-d05y.onrender.com/todos/';
-import axios from 'axios';
 import Todo from './Todo';
+import axios from 'axios';
+const apiUrl = 'https://todo-api-d05y.onrender.com/todos/';
+
+
 function Home() {
     let user = {};
     if (localStorage.getItem('todoUser')) {
-
         let data = JSON.parse(localStorage.getItem('todoUser'));
         user.id = data.userId
         user.name = data.username;
     }
+
     const [todos, settodos] = useState([])
     const [isOngoing, setisOngoing] = useState(false);
     const [ongoingErr, setongoingErr] = useState(false)
     let [addActivity, setActivity] = useState('')
 
     async function getTodos() {
-
         await axios.get(apiUrl + user.id).then(res => {
             settodos(res.data.todos)
             let ongoing = res.data.todos.filter(todo => todo.status == 'Ongoing')
             setisOngoing(ongoing.length > 0);
         }).catch(console.log);
-
-
     }
     useEffect(() => {
         getTodos();
     }, [])
     async function handleAdd() {
-        let res = await axios.post(apiUrl + user.id, { activity: addActivity }).catch(console.log);
-
-        setActivity('');
-        getTodos();
+        await axios.post(apiUrl + user.id, { activity: addActivity }).then(() => {
+            setActivity('');
+            getTodos();
+        }).catch(console.log);
     }
-
     return (
         <>  { !localStorage.getItem('todoUser') ? <Navigate to='/login' /> :
             <div className="home">
                 <Nav user={ user } />
                 <SideBar todos={ todos } />
-                <main className='ml-56 pl-28 mt-48'>
-                    { ongoingErr && <p className='text-center text-red-500'>Finish or Pause the ongoing task first.</p> }
+                <main className='lg:ml-56 lg:pl-28 mt-48'>
+                    { ongoingErr && <p className='text-center text-3xl font-semibold text-red-600'>Finish or Pause the ongoing task first.</p> }
                     <div className="addtask flex gap-4 justify-center m-4">
                         <input className='p-2 rounded-md focus:outline-green-500' type="text" value={ addActivity } onChange={ (e) => setActivity(e.target.value) } name="activity" id="" placeholder='Task Name' />
                         <button disabled={ addActivity.length == 0 } className='p-2 bg-violet-500 hover:bg-violet-800 rounded-md disabled:bg-violet-500 disabled:cursor-not-allowed' onClick={ handleAdd }>Add activity</button>
@@ -61,11 +59,9 @@ function Home() {
                             </thead>
                             <tbody className=''>
                                 { todos.map((todo, i) => (
-                                    <Todo refresh={ getTodos } id={ todo._id } isOngoing={ isOngoing } setisOngoing={ setisOngoing } key={ i } todo={ todo } setongoingErr={ setongoingErr } />
+                                    <Todo refresh={ getTodos } id={ todo._id } isOngoing={ isOngoing } setisOngoing={ setisOngoing } key={ todo._id } todo={ todo } setongoingErr={ setongoingErr } />
                                 )) }
-                            </tbody>
-
-                        </table>
+                            </tbody></table>
                     </div>
                 </main>
             </div>
